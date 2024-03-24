cdfa94bad90dc837e76fe406f0a5b38af67c7103
TalesNotTails <c.jetta2014@gmail.com>
2024-03-24 13:21:55 -0400

    Created scraper for dice.com

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
+const scrapeDice = require("./scrapers/scrapeDice");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -7,4 +8,22 @@ client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.login(token);
+// client.login(token);
+
+// Build stuff for web scraper
+
+const params = {
+  q: "devops",
+  location: "Wilmington, DE, USA",
+  page: 1,
+  pageSize: 100,
+  "filters.postedDate": "ONE",
+  "filters.workplaceTypes": "Remote",
+  language: "en",
+};
+
+scrapeDice(params).then((links) => {
+  links.map((link) => {
+    console.log(`https://www.dice.com/job-detail/${link}`);
+  });
+});
